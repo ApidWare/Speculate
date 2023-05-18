460163ec07d3b74f04a251c7587c9c4672ef8e6c
ApidBoy <samarthsb@duck.com>
2023-05-18 10:03:28 +0530

    Initial commit

--- client/src/components/homepageComponents/Card.js
+++ client/src/components/homepageComponents/Card.js
@@ -1,18 +1,23 @@
 import React from "react";
 function Card(props) {
     return(
-        <section id="card">
-            <img id="cardImage" src={props.image} />
-            <div id="cardTitle">
-                {props.title}
-            </div>
-            <div id="cardDescription">
-                {props.description}
-            </div>
+        <div id="cardParent">
             <div id="cardAuthor">
+                <img id="authorImage" src={props.authorImage}/>
                 {props.author}
             </div>
-        </section>
+            <section id="card">
+                <img id="cardImage" src={props.image} />
+                <div id="cardInfo">
+                    <div id="cardTitle">
+                        {props.title}
+                    </div>
+                    <div id="cardDescription">
+                        {props.description}
+                    </div>
+                </div>
+            </section>
+        </div>
     );
 }
 export default Card;
--- client/src/components/homepageComponents/CardContainer.js
+++ client/src/components/homepageComponents/CardContainer.js
@@ -2,26 +2,31 @@ import React from "react";
 import Card from "./Card";
 function CardContainer() {
     return(
-        <section id="cardContainer">
-            <Card
-                image="https://occ-0-55-37.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABa6xunmQtNXxRz3beRkEjyhWGjDWtLREoRGEi8n4aAe5kg_KKDbZ58BUwLDj3FX80kb1fb3oP9Or7jKNDIHcd7esJ5x2Az6qIyYxIiQhB60R2I1onFt0NUSQsrSWJLQRw8lGnA.jpg?r=1aa"
-                title="The Invisible Hand: Big Tech's Control Over Our Lives"
-                description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
-                author="Sunny Singh"
-            />
-            <Card
-                image="https://images.livemint.com/img/2022/07/06/1600x900/5g_1652299257638_1657124003796.jpg"
-                title=""
-                description=""
-                author=""
-            />
-            <Card
-                image="https://cloud2data.com/wp-content/uploads/2023/02/AI-7.jpg"
-                title=""
-                description=""
-                author=""
-            />
-        </section>
+        <div id="cardSection">
+            <section id="cardContainer">
+                <Card
+                    image="https://occ-0-55-37.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABa6xunmQtNXxRz3beRkEjyhWGjDWtLREoRGEi8n4aAe5kg_KKDbZ58BUwLDj3FX80kb1fb3oP9Or7jKNDIHcd7esJ5x2Az6qIyYxIiQhB60R2I1onFt0NUSQsrSWJLQRw8lGnA.jpg?r=1aa"
+                    title="The Invisible Hand: Big Tech's Control Over Our Lives"
+                    description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s and even more dummy text is on the way."
+                    author="Sunny Singh"
+                    authorImage="https://i.ibb.co/BgtmVkX/sunny.jpg"
+                />
+                <Card
+                    image="https://images.livemint.com/img/2022/07/06/1600x900/5g_1652299257638_1657124003796.jpg"
+                    title="5G, A myth in India - Why 5G will never be real in countries like India"
+                    description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s and even more dummy text."
+                    author="Richa Parihar"
+                    authorImage="https://i.ibb.co/26vghhD/richa.jpg"
+                />
+                <Card
+                    image="https://cloud2data.com/wp-content/uploads/2023/02/AI-7.jpg"
+                    title="The AI Revolution: Impact of Artificial Intelligence on our lives"
+                    description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s and even more."
+                    author="Riya Motwani"
+                    authorImage="https://i.ibb.co/2S7p89N/riya.jpg"
+                />
+            </section>
+        </div>
     );
 }
 export default CardContainer;
